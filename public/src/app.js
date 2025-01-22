026c7235be5733aa63d1620d501e03426d5f2604
Kuma Kang <tpfprkem92@naver.com>
2025-01-22 14:51:00 +0900

    commit6

--- public/src/app.js
+++ public/src/app.js
@@ -20,37 +20,61 @@ const btnAddRow = document.querySelector(".btnAddRow");
 const btnDeleteRow = document.querySelector(".btnDeleteRow");
 const btnAddColumn = document.querySelector(".btnAddColumn");
 const btnDeleteColumn = document.querySelector(".btnDeleteColumn");
+const btnHideTable = document.querySelector(".btnHideTable");
+const selectScreenRatio = document.querySelector(".selectScreenRatio");
+const textCell = document.querySelectorAll(".textCell");
+const markCurrentCell = document.querySelector(".markCurrentCell");
 let currentPageOrder;
 let tempPageOrder;
 let currentPage;
+let clickedTable;
+//
 let currentTable;
+let currentCell;
+let colIdx;
+let rowIdx;
+let lastCol;
+let lastRow;
 /////////////////////////////////////////////////////////////////////////////////////////////////// 함수
 /////////////////////////////////////////////////////////HEADER////////////////////////////////////////
 // 현재 테이블 확인
-function findCurrentTable(event) {
+function findClickedTable(event) {
   if (document.querySelector(".tableClicked")) {
-    currentTable = document.querySelector(".tableClicked");
-    if (currentTable === event.target.parentNode.parentNode) {
-      currentTable.classList.remove("tableClicked");
-      currentTable = "";
+    clickedTable = document.querySelector(".tableClicked");
+    if (clickedTable === event.target.parentNode.parentNode) {
+      clickedTable.classList.remove("tableClicked");
+      clickedTable = "";
     } else {
-      currentTable.classList.remove("tableClicked");
-      currentTable = event.target.parentNode.parentNode;
-      currentTable.classList.add("tableClicked");
+      clickedTable.classList.remove("tableClicked");
+      clickedTable = event.target.parentNode.parentNode;
+      clickedTable.classList.add("tableClicked");
     }
   } else {
-    currentTable = event.target.parentNode.parentNode;
-    currentTable.classList.add("tableClicked");
+    clickedTable = event.target.parentNode.parentNode;
+    clickedTable.classList.add("tableClicked");
   }
 }
-//오늘 날짜로
-function goToday() {
+//헤더 초기화화
+function headerInit() {
+  //날짜짜
   const today = new Date();
   const yyyy = today.getFullYear();
   const mm = String(today.getMonth() + 1).padStart(2, "0");
   const dd = String(today.getDate()).padStart(2, "0");
   const formattedDate = `${yyyy}-${mm}-${dd}`;
   selectDate.value = formattedDate;
+  //시간
+  selectTime.value = "09:00";
+  //배율
+  if (currentPage) {
+    if (currentPage.style.transform === "") {
+      selectScreenRatio.value = "100%";
+    } else {
+      selectScreenRatio.value = currentPage.style.transform.slice(6).slice(0, -1) * 100 + "%";
+    }
+  } else {
+    selectScreenRatio.value = "100%";
+  }
 }
 //테이블 날짜 세팅
 function settingDate() {
@@ -63,9 +87,9 @@ function settingDate() {
       currentPage.querySelectorAll(".dayTable")[i].querySelector(".timeCell").innerText = newDate.toISOString().split("T")[0].slice(5) + ` (${week[i]})`;
     }
   }
-  if (currentTable) {
-    currentTable.classList.remove("tableClicked");
-    currentTable = "";
+  if (clickedTable) {
+    clickedTable.classList.remove("tableClicked");
+    clickedTable = "";
   }
 }
 //테이블 시간 세팅
@@ -76,7 +100,7 @@ function settingTime() {
 
   //인덱스 찾기
   let timeIndex = time.indexOf(selectTime.value);
-  if (!currentTable) {
+  if (!clickedTable) {
     //전체 테이블블
     for (let i = 0; i < 7; i++) {
       for (let j = 1; j <= currentPage.querySelectorAll(".dayTable")[i].querySelectorAll(".timeCell").length - 1; j++) {
@@ -89,90 +113,118 @@ function settingTime() {
     }
   } else {
     //한개 테이블블
-    for (let j = 1; j <= currentTable.querySelectorAll(".timeCell").length - 1; j++) {
+    for (let j = 1; j <= clickedTable.querySelectorAll(".timeCell").length - 1; j++) {
       if (time[timeIndex + j] === undefined) {
-        currentTable.querySelectorAll(".timeCell")[j].innerText = "";
+        clickedTable.querySelectorAll(".timeCell")[j].innerText = "";
       } else {
-        currentTable.querySelectorAll(".timeCell")[j].innerText = time[timeIndex + j - 1] + " - " + time[timeIndex + j];
+        clickedTable.querySelectorAll(".timeCell")[j].innerText = time[timeIndex + j - 1] + " - " + time[timeIndex + j];
       }
     }
   }
 }
-
+//테이블 숨기기기
+function hideTable() {
+  if (!clickedTable) {
+    return;
+  }
+  if (clickedTable.querySelector(".textColumn")) {
+    if (clickedTable.querySelector(".textColumn").classList.contains("hide")) {
+      clickedTable.querySelectorAll(".textColumn").forEach((cols) => {
+        cols.classList.remove("hide");
+      });
+    } else {
+      clickedTable.querySelectorAll(".textColumn").forEach((cols) => {
+        cols.classList.add("hide");
+      });
+    }
+  }
+}
 function addCol() {
   if (!currentPage) {
     return;
   }
 
-  if (currentTable) {
+  if (clickedTable) {
     //테이블 하나
     let newTextColumn = document.createElement("div");
     newTextColumn.classList.add("textColumn");
-    for (let j = 0; j < currentTable.querySelectorAll(".timeCell").length; j++) {
+    let numOfTextColumn = clickedTable.querySelectorAll(".textColumn").length;
+    for (let j = 0; j < clickedTable.querySelectorAll(".timeCell").length; j++) {
       let newTextCell = document.createElement("div");
       newTextCell.classList.add("textCell");
       newTextCell.setAttribute("contenteditable", true);
+      newTextCell.setAttribute("rowIndex", j);
+      newTextCell.setAttribute("colIndex", numOfTextColumn);
+      newTextCell.addEventListener("keydown", cellMove);
+      newTextCell.addEventListener("click", findCurrentCell);
       newTextColumn.appendChild(newTextCell);
     }
-    currentTable.appendChild(newTextColumn);
+    clickedTable.appendChild(newTextColumn);
   } else {
     //테이블 전체
     for (let i = 0; i < 7; i++) {
       let numOfRow = currentPage.querySelectorAll(".dayTable")[i].querySelectorAll(".timeCell").length;
       let newTextColumn = document.createElement("div");
       newTextColumn.classList.add("textColumn");
+      let numOfTextColumn = document.querySelectorAll(".dayTable")[i].querySelectorAll(".textColumn").length;
       for (let j = 0; j < numOfRow; j++) {
         let newTextCell = document.createElement("div");
         newTextCell.classList.add("textCell");
         newTextCell.setAttribute("contenteditable", true);
+        newTextCell.setAttribute("rowIndex", j);
+        newTextCell.setAttribute("colIndex", numOfTextColumn);
+        newTextCell.addEventListener("keydown", cellMove);
+        newTextCell.addEventListener("click", findCurrentCell);
         newTextColumn.appendChild(newTextCell);
       }
       currentPage.querySelectorAll(".dayTable")[i].appendChild(newTextColumn);
     }
   }
 }
-
 function deleteCol() {
   if (!currentPage) {
     return;
   }
-
-  if (currentTable) {
+  if (clickedTable) {
     //한개 테이블
-    if (currentTable.querySelector(".timeColumn")) {
-      let numOfCol = currentTable.querySelectorAll(".timeColumn").length;
-      currentTable.querySelectorAll(".textColumn")[numOfCol - 1].remove();
+    if (clickedTable.querySelector(".textColumn")) {
+      let numOfCol = clickedTable.querySelectorAll(".timeColumn").length;
+      clickedTable.querySelectorAll(".textColumn")[numOfCol - 1].remove();
     }
   } else {
     //전체 테이블
     for (let i = 0; i < 7; i++) {
-      if (currentPage.querySelectorAll(".dayTable")[i].querySelector(".timeColumn")) {
+      if (currentPage.querySelectorAll(".dayTable")[i].querySelector(".textColumn")) {
         let numOfCol = currentPage.querySelectorAll(".dayTable")[i].querySelectorAll(".timeColumn").length;
         currentPage.querySelectorAll(".dayTable")[i].querySelectorAll(".textColumn")[numOfCol - 1].remove();
       }
     }
   }
 }
-
 function addRow() {
   if (!currentPage) {
     return;
   }
 
-  if (currentTable) {
+  if (clickedTable) {
     //한개 테이블
     //timeCell
     let newTimeCell = document.createElement("div");
     newTimeCell.classList.add("timeCell");
-    currentTable.querySelector(".timeColumn").appendChild(newTimeCell);
+    clickedTable.querySelector(".timeColumn").appendChild(newTimeCell);
 
     //textCell
-    let numOfCol = currentTable.querySelectorAll(".textColumn").length;
+    let numOfCol = clickedTable.querySelectorAll(".textColumn").length;
+    let numOfRow = clickedTable.querySelectorAll(".timeCell").length - 1;
     for (let j = 0; j < numOfCol; j++) {
       let newTextCell = document.createElement("div");
       newTextCell.classList.add("textCell");
       newTextCell.setAttribute("contenteditable", true);
-      currentTable.querySelectorAll(".textColumn")[j].appendChild(newTextCell);
+      newTextCell.setAttribute("rowIndex", numOfRow);
+      newTextCell.setAttribute("colIndex", j);
+      newTextCell.addEventListener("keydown", cellMove);
+      newTextCell.addEventListener("click", findCurrentCell);
+      clickedTable.querySelectorAll(".textColumn")[j].appendChild(newTextCell);
     }
   } else {
     //전체 테이블
@@ -184,48 +236,103 @@ function addRow() {
 
       //textCell
       let numOfCol = currentPage.querySelectorAll(".dayTable")[i].querySelectorAll(".textColumn").length;
+      let numOfRow = currentPage.querySelectorAll(".dayTable")[i].querySelectorAll(".timeCell").length - 1;
       for (let j = 0; j < numOfCol; j++) {
         let newTextCell = document.createElement("div");
         newTextCell.classList.add("textCell");
         newTextCell.setAttribute("contenteditable", true);
+        newTextCell.setAttribute("rowIndex", numOfRow);
+        newTextCell.setAttribute("colIndex", j);
+        newTextCell.addEventListener("keydown", cellMove);
+        newTextCell.addEventListener("click", findCurrentCell);
         currentPage.querySelectorAll(".dayTable")[i].querySelectorAll(".textColumn")[j].appendChild(newTextCell);
       }
     }
   }
 }
-
 function deleteRow() {
   if (!currentPage) {
     return;
   }
 
-  if (currentTable) {
+  if (clickedTable) {
     //한개 테이블
     //timeCell
-    let numOfRow = currentTable.querySelectorAll(".timeCell").length;
-    currentTable.querySelector(".timeColumn").querySelectorAll(".timeCell")[numOfRow - 1].remove();
-
+    let numOfRow = clickedTable.querySelectorAll(".timeCell").length;
+    if (numOfRow < 2) {
+      return;
+    }
+    clickedTable.querySelector(".timeColumn").querySelectorAll(".timeCell")[numOfRow - 1].remove();
     //textCell
-    let numOfCol = currentTable.querySelectorAll(".textColumn").length;
+    let numOfCol = clickedTable.querySelectorAll(".textColumn").length;
     for (let j = 0; j < numOfCol; j++) {
-      currentTable.querySelectorAll(".textColumn")[j].querySelectorAll(".textCell")[numOfRow - 1].remove();
+      clickedTable.querySelectorAll(".textColumn")[j].querySelectorAll(".textCell")[numOfRow - 1].remove();
     }
   } else {
     //전체 테이블
     for (let i = 0; i < 7; i++) {
       //timeCell
       let numOfRow = currentPage.querySelectorAll(".dayTable")[i].querySelectorAll(".timeCell").length;
-      currentPage.querySelectorAll(".dayTable")[i].querySelector(".timeColumn").querySelectorAll(".timeCell")[numOfRow - 1].remove();
+      if (numOfRow > 1) {
+        currentPage.querySelectorAll(".dayTable")[i].querySelector(".timeColumn").querySelectorAll(".timeCell")[numOfRow - 1].remove();
 
-      //textCell
-      let numOfCol = currentPage.querySelectorAll(".dayTable")[i].querySelectorAll(".textColumn").length;
-      for (let j = 0; j < numOfCol; j++) {
-        currentPage.querySelectorAll(".dayTable")[i].querySelectorAll(".textColumn")[j].querySelectorAll(".textCell")[numOfRow - 1].remove();
+        //textCell
+        let numOfCol = currentPage.querySelectorAll(".dayTable")[i].querySelectorAll(".textColumn").length;
+        for (let j = 0; j < numOfCol; j++) {
+          currentPage.querySelectorAll(".dayTable")[i].querySelectorAll(".textColumn")[j].querySelectorAll(".textCell")[numOfRow - 1].remove();
+        }
       }
     }
   }
 }
+function adjustScreenRatio() {
+  if (!currentPage) {
+    return;
+  }
+  let abc = selectScreenRatio.value.slice(0, -1) * 1 * 0.01;
+  currentPage.style.transform = `scale(${abc})`;
+}
 /////////////////////////////////////////////////////////MAIN///////////////////////////////////////////////////////////
+function findCurrentCell(event) {
+  currentCell = event.target;
+  currentTable = currentCell.parentNode.parentNode;
+  lastCol = currentTable.querySelectorAll(".textColumn").length - 1;
+  lastRow = currentTable.querySelectorAll(".timeCell").length - 1;
+  colIdx = Number(currentCell.getAttribute("colIndex"));
+  rowIdx = Number(currentCell.getAttribute("rowIndex"));
+  //표시
+  markCurrentCell.innerText = "";
+  markCurrentCell.innerText = rowIdx + ", " + colIdx;
+}
+function cellMove(event) {
+  if (event.ctrlKey) {
+    // 현재칸 다음칸의 '빈칸' or '글자칸'에 따라 이동경로 정하기
+    console.log("ctrl");
+  }
+  if (event.shiftKey) {
+    // 드래그처럼 셀 연속블럭잡기기
+    console.log("shift");
+  }
+  if (event.ctrlKey && event.shiftKey) {
+    // 알지? 이거 ctrl 밑에 shift 넣어야할듯?
+    console.log("ctrl + shift");
+  }
+
+  if (event.key === "ArrowUp" && rowIdx > 1) {
+    rowIdx--;
+  } else if (event.key === "ArrowDown" && rowIdx < lastRow) {
+    rowIdx++;
+  } else if (event.key === "ArrowLeft" && colIdx > 0) {
+    colIdx--;
+  } else if (event.key === "ArrowRight" && colIdx < lastCol) {
+    colIdx++;
+  }
+  currentCell = currentTable.querySelectorAll(".textColumn")[colIdx].querySelectorAll(".textCell")[rowIdx];
+  currentCell.focus();
+  //표시
+  markCurrentCell.innerText = "";
+  markCurrentCell.innerText = rowIdx + ", " + colIdx;
+}
 
 /////////////////////////////////////////////////////////FOOTER///////////////////////////////////////////////////////////
 // 버튼 좌클릭시 버튼+페이지 추가
@@ -252,7 +359,6 @@ function addPage() {
   // 클릭전까지 페이지 숨기기
   newPage.classList.add("hide");
 }
-
 // 버튼 좌클릭시 페이지 전환 or 페이지 내 테이블 생성
 function openPage(event) {
   currentPageOrder = event.target.getAttribute("order");
@@ -277,7 +383,7 @@ function openPage(event) {
 
       const newTimeColumn = document.createElement("div");
       newTimeColumn.classList.add("timeColumn");
-      newTimeColumn.addEventListener("click", findCurrentTable);
+      newTimeColumn.addEventListener("click", findClickedTable);
       newTable.appendChild(newTimeColumn);
 
       for (let j = 0; j < 26; j++) {
@@ -290,9 +396,7 @@ function openPage(event) {
     }
   }
 
-  //달력날짜 초기화
-  // selectDate.value = "";
-  goToday();
+  headerInit();
 }
 // 버튼 우클릭시 모달 띄우기 - 이름설정 / 버튼+페이지 삭제
 function openModal(event) {
@@ -329,7 +433,7 @@ function closePreventModal(event) {
 // ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 이벤트
 // ////////////////////////////////////////////////////////////////////////////////////////////////////////
-goToday();
+// headerInit();
 btnAddPage.addEventListener("click", addPage);
 modalDeleteBtn.addEventListener("click", modalDelete);
 modalNameBtn.addEventListener("click", modalNaming);
@@ -342,3 +446,5 @@ btnAddColumn.addEventListener("click", addCol);
 btnDeleteColumn.addEventListener("click", deleteCol);
 btnAddRow.addEventListener("click", addRow);
 btnDeleteRow.addEventListener("click", deleteRow);
+btnHideTable.addEventListener("click", hideTable);
+selectScreenRatio.addEventListener("change", adjustScreenRatio);
